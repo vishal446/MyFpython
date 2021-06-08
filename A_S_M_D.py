58f7ed5d5f9786c26d68e57448c4b6bbf5d0c727
vishal446 <vx857276@gmail.com>
2021-06-08 09:13:58 +0530

    Add, Subtraction, Multiplication, Division program file

---
+++ A_S_M_D.py
@@ -0,0 +1,10 @@
+_Num1=int(input("Enter first Number:"))
+_Num2=int(input("Enter second Number:"))
+_Sum=_Num1+_Num2
+print("the sum of two Numbers are:",_Sum)
+_Sub=_Num1-_Num2
+print("The subtraction of two numbers are:",_Sub)
+_Mux=_Num1*_Num2
+print("The multiplication of two numbers are:",_Mux)
+_Div=_Num1/_Num2
+print("The division of two numbers is:",_Div)
